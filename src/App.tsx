dd3b126da1067a0585fa0ec55007b225325470ff
Iris Adina Morar <morariris@gmail.com>
2026-03-19 17:16:12 +0200

    fix the edge cases for winning and losing the game, create the tutorial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,85 @@
 import "./App.css";
+import { useCardsLogic } from "./useCardsLogic";
 
-export default function App() {}
+export default function App() {
+  const {
+    page,
+    selectedBoardSize,
+    board,
+    flippedUpCardIndices,
+    playerMovesCount,
+    secondsElapsed,
+    setPageAtPlay,
+    setPageAtFinish,
+    set4x4BoardSize,
+    set6x6BoardSize,
+    set8x8BoardSize,
+    set4x4Board,
+    set6x6Board,
+    set8x8Board,
+    flipUpCard,
+    reset,
+  } = useCardsLogic();
+
+  return (
+    <main>
+      {page === "tutorial" && (
+        <section>
+          <section className="tutorial_text">
+            <h1>Welcome to the Memory Cards Game</h1>
+            <h3>where your brain gets a workout and your luck gets judged</h3>
+            <p>
+              Flip two cards at a time and try to find matching pairs. If they
+              match, congrats, you’re officially smarter than you were 2 seconds
+              ago. If not… well, at least you tried.
+            </p>
+            <p>
+              But don’t get too comfortable! Each level comes with its own
+              “tick-tock pressure”:
+            </p>
+            <p>4×4 grid → 60 seconds & maximum 19 moves</p>
+            <p>6×6 grid → 140 seconds & maximum 39 moves</p>
+            <p>8×8 grid → 220 seconds & maximum 67 moves</p>
+            <p>
+              Yes, we’re timing you. Yes, we’re counting your moves. No, we’re
+              not judging… okay, maybe a little. Stay within the limits if you
+              want to win or risk eternal shame (and a restart button).
+            </p>{" "}
+            <br />
+            <p>Good luck, memory master. Try not to forget why you’re here!</p>
+          </section>
+          <section className="board_size_buttons_container">
+            <button
+              onClick={() => {
+                setPageAtPlay();
+                set4x4Board();
+                set4x4BoardSize();
+              }}
+            >
+              4x4
+            </button>
+            <button
+              onClick={() => {
+                setPageAtPlay();
+                set6x6Board();
+                set6x6BoardSize();
+              }}
+            >
+              6x6
+            </button>
+            <button
+              onClick={() => {
+                setPageAtPlay();
+                set8x8Board();
+                set8x8BoardSize();
+              }}
+            >
+              8x8
+            </button>
+          </section>
+        </section>
+      )}
+      {page==="play" && ()}
+    </main>
+  );
+}
--- src/useCardsLogic.ts
+++ src/useCardsLogic.ts
@@ -1,10 +1,10 @@
-import { useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-const CARD_SYMBOLS_4x4: string[] = ["B", "8", "₿", "🤍", "✨", "💭", "🌙", "฿"];
+const CARD_SYMBOLS_4x4: string[] = ["B", "8", "þ", "🤍", "✨", "💭", "🌙", "฿"];
 const CARD_SYMBOLS_6x6: string[] = [
   "B",
   "8",
-  "₿",
+  "þ",
   "🤍",
   "✨",
   "💭",
@@ -12,19 +12,19 @@ const CARD_SYMBOLS_6x6: string[] = [
   "฿",
   "🌟",
   "☁️",
-  "🩶",
-  "β",
+  "😊",
+  "P",
   "⊕",
   "⊗",
-  "⊙",
+  "🐣",
   "☀️",
-  "🌞",
+  "💛",
   "⚪",
 ];
 const CARD_SYMBOLS_8x8: string[] = [
   "B",
   "8",
-  "₿",
+  "þ",
   "🤍",
   "✨",
   "💭",
@@ -32,13 +32,13 @@ const CARD_SYMBOLS_8x8: string[] = [
   "฿",
   "🌟",
   "☁️",
-  "🩶",
-  "β",
+  "😊",
+  "P",
   "⊕",
   "⊗",
-  "⊙",
+  "🐣",
   "☀️",
-  "🌞",
+  "💛",
   "⚪",
   "∅",
   "ℚ",
@@ -57,14 +57,14 @@ const CARD_SYMBOLS_8x8: string[] = [
 ];
 
 const MAX_PLAYER_MOVES_4x4 = 19;
-const MAX_PLAYER_MOVES_6x6 = 35;
-const MAX_PLAYER_MOVES_8x8 = 60;
+const MAX_PLAYER_MOVES_6x6 = 39;
+const MAX_PLAYER_MOVES_8x8 = 67;
 
 const TOTAL_SECONDS_4x4 = 60;
 const TOTAL_SECONDS_6x6 = 120;
 const TOTAL_SECONDS_8x8 = 180;
 
-export function useCards() {
+export function useCardsLogic() {
   const [page, setPage] = useState<"tutorial" | "play" | "finish">("tutorial");
   const [selectedBoardSize, setSelectedBoardSize] = useState<
     "4x4" | "6x6" | "8x8" | null
@@ -80,6 +80,60 @@ export function useCards() {
   const [secondsElapsed, setSecondsElapsed] = useState(0);
   const timerRef = useRef<number | null>(null);
 
+  const setPageAtPlay = useCallback(() => {
+    setPage("play");
+  }, []);
+
+  const setPageAtFinish = useCallback(() => {
+    setPage("finish");
+  }, []);
+
+  const set4x4BoardSize = useCallback(() => {
+    setSelectedBoardSize("4x4");
+  }, []);
+
+  const set6x6BoardSize = useCallback(() => {
+    setSelectedBoardSize("6x6");
+  }, []);
+
+  const set8x8BoardSize = useCallback(() => {
+    setSelectedBoardSize("8x8");
+  }, []);
+
+  const set4x4Board = useCallback(() => {
+    setBoard(CARD_SYMBOLS_4x4);
+  }, []);
+
+  const set6x6Board = useCallback(() => {
+    setBoard(CARD_SYMBOLS_6x6);
+  }, []);
+
+  const set8x8Board = useCallback(() => {
+    setBoard(CARD_SYMBOLS_8x8);
+  }, []);
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setSecondsElapsed((prev) => {
+        if (selectedBoardSize === "4x4" && prev === TOTAL_SECONDS_4x4) {
+          setPage("finish");
+        }
+        if (selectedBoardSize === "6x6" && prev === TOTAL_SECONDS_6x6) {
+          setPage("finish");
+        }
+        if (selectedBoardSize === "8x8" && prev === TOTAL_SECONDS_8x8) {
+          setPage("finish");
+        }
+        return prev + 1;
+      });
+    }, 1000);
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, [selectedBoardSize]);
+
   const flipUpCard = (cardIndex: number) => {
     if (page !== "play") {
       return;
@@ -91,6 +145,15 @@ export function useCards() {
     }
     if (currentCardPairIndices.length === 1) {
       setPlayerMovesCount((prev) => {
+        if (selectedBoardSize === "4x4" && prev === MAX_PLAYER_MOVES_4x4) {
+          setPage("finish");
+        }
+        if (selectedBoardSize === "6x6" && prev === MAX_PLAYER_MOVES_6x6) {
+          setPage("finish");
+        }
+        if (selectedBoardSize === "8x8" && prev === MAX_PLAYER_MOVES_8x8) {
+          setPage("finish");
+        }
         return prev + 1;
       });
       const firstCardIndex = currentCardPairIndices[0];
@@ -102,34 +165,71 @@ export function useCards() {
           return [...prev, firstCardIndex, secondCardIndex];
         });
         setCurrentCardPairIndices([]);
+        if (selectedBoardSize === "4x4" && flippedUpCardIndices.length === 16) {
+          setPage("finish");
+        }
+        if (selectedBoardSize === "6x6" && flippedUpCardIndices.length === 36) {
+          setPage("finish");
+        }
+        if (selectedBoardSize === "8x8" && flippedUpCardIndices.length === 64) {
+          setPage("finish");
+        }
+      } else {
+        setTimeout(() => {
+          setCurrentCardPairIndices([]);
+        }, 700);
       }
     }
   };
 
-  const hasPlayerWon = () => {
-    switch (selectedBoardSize) {
-      case "4x4": {
-        return playerMovesCount < MAX_PLAYER_MOVES_4x4 && timerRef.current != 0;
-      }
-      case "6x6": {
-        return playerMovesCount < MAX_PLAYER_MOVES_6x6 && timerRef.current != 0;
-      }
-      case "8x8": {
-        return playerMovesCount < MAX_PLAYER_MOVES_8x8 && timerRef.current != 0;
-      }
-      default:
-        return false;
-    }
+  // const hasPlayerWon = () => {
+  //   switch (selectedBoardSize) {
+  //     case "4x4": {
+  //       return (
+  //         playerMovesCount < MAX_PLAYER_MOVES_4x4 &&
+  //         secondsElapsed < TOTAL_SECONDS_4x4
+  //       );
+  //     }
+  //     case "6x6": {
+  //       return (
+  //         playerMovesCount < MAX_PLAYER_MOVES_6x6 &&
+  //         secondsElapsed < TOTAL_SECONDS_6x6
+  //       );
+  //     }
+  //     case "8x8": {
+  //       return (
+  //         playerMovesCount < MAX_PLAYER_MOVES_8x8 &&
+  //         secondsElapsed < TOTAL_SECONDS_8x8
+  //       );
+  //     }
+  //     default:
+  //       return false;
+  //   }
+  // };
 
-    return {
-      page,
-      selectedBoardSize,
-      board,
-      flippedUpCardIndices,
-      playerMovesCount,
-      secondsElapsed,
-      flipUpCard,
-      hasPlayerWon,
-    };
+  const reset = useCallback(() => {
+    setPage("tutorial");
+    setFlippedUpCardIndices([]);
+    setCurrentCardPairIndices([]);
+    setSecondsElapsed(0);
+  }, []);
+
+  return {
+    page,
+    selectedBoardSize,
+    board,
+    flippedUpCardIndices,
+    playerMovesCount,
+    secondsElapsed,
+    setPageAtPlay,
+    setPageAtFinish,
+    set4x4BoardSize,
+    set6x6BoardSize,
+    set8x8BoardSize,
+    set4x4Board,
+    set6x6Board,
+    set8x8Board,
+    flipUpCard,
+    reset,
   };
 }
